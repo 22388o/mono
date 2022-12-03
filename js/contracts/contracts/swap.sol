85eccb648a23160ddf83e2f5af19434fdef73ab8
Anand Suresh <anandsuresh@gmail.com>
2022-12-02 17:45:43 -0800

    npm test works; truffle deploy works; nix-build does not

--- js/contracts/contracts/swap.sol
+++ js/contracts/contracts/swap.sol
@@ -1,4 +1,5 @@
 /// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.5.8;
 
 
 /// ERC-20 Interface
--- js/contracts/truffle-config.js
+++ js/contracts/truffle-config.js
@@ -2,28 +2,38 @@
  * @file Configuration for the Truffle framework
  */
 
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const MNEMONIC = 'maple bag hire erode journey winter midnight mail protect range diagram kidney'
+const INFURA_API_KEY = '3f6691a33225484c8e1eebde034b274f'
+
+const configPath = process.env.TRUFFLE_CONF_PATH
+  ? process.env.TRUFFLE_CONF_PATH
+  : './truffle-config.json'
+
 /**
  * Export the configuration
  * @type {Object}
  */
-const fs = require('fs');
-
-const configPath = process.env.TRUFFLE_CONF_PATH ? process.env.TRUFFLE_CONF_PATH : './truffle-config.json';
-const Config = module.exports = JSON.parse(fs.readFileSync(configPath));
-/**
- * Flag checking if debugging is enabled
- * @type {Boolean}
- */
-const debugEnabled = Object.values(process.argv).includes('--debug')
-
-/**
- * Start a test blockchain instance
- */
-require('ganache')
-  .server({
-    logging: {
-      quiet: !debugEnabled,
-      verbose: debugEnabled
+const Config = module.exports = Object.assign({}, require(configPath), {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*'
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${INFURA_API_KEY}`),
+      network_id: '5',
+      gas: 4465030
+    },
+    sepolia: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://sepolia.infura.io/v3/${INFURA_API_KEY}`),
+      network_id: '11155111',
+      gas: 4465030
     }
-  })
+  }
+})
+
+require('ganache')
+  .server()
   .listen(Config.networks.development.port)
