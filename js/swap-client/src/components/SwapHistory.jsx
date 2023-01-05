210a9b1e45d9d4e702c49305dd60d596810c0485
victorwu <victorwu@users.noreply.github.com>
2023-01-04 21:59:59 -0800

    feature: SwapHistory table

--- js/swap-client/src/components/SwapHistory.jsx
+++ js/swap-client/src/components/SwapHistory.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { Header, Table, Rating } from 'semantic-ui-react'
+import { useAppSelector } from '../hooks';
 
 export const SwapHistory = () => {
-  return ( <div>Swap History</div> );
+  const history = useAppSelector(state => state.history.history);
+
+  return (
+    <Table celled padded>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell singleLine>Base Amount</Table.HeaderCell>
+          <Table.HeaderCell singleLine>Quote Amount</Table.HeaderCell>
+          <Table.HeaderCell singleLine>Swap Id</Table.HeaderCell>
+          <Table.HeaderCell>Hash</Table.HeaderCell>
+          <Table.HeaderCell singleLine>Secret Seeker Id</Table.HeaderCell>
+          <Table.HeaderCell singleLine>Secret Holder Id</Table.HeaderCell>
+          <Table.HeaderCell>Secret</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        {
+          history.map(row => 
+            <Table.Row>
+              <Table.Cell>{row.amountBase}</Table.Cell>
+              <Table.Cell>{row.amountQuote}</Table.Cell>
+              <Table.Cell>{row.swapId}</Table.Cell>
+              <Table.Cell>{row.swapHash}</Table.Cell>
+              <Table.Cell>{row.secretSeekerId}</Table.Cell>
+              <Table.Cell>{row.secretHolderId}</Table.Cell>
+              <Table.Cell>{row.secret}</Table.Cell>
+            </Table.Row>
+          )
+        }
+      </Table.Body>
+    </Table>
+  );
 }
