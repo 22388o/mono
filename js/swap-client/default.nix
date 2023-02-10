d88c919ab3eb9f85c22fcd39894bccddbe839ce3
Anand Suresh <anandsuresh@gmail.com>
2023-02-10 08:49:43 -0800

    chore: build the UI using nix

---
+++ js/swap-client/default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import ../../nix { inherit system; }
+, system ? builtins.currentSystem
+, nodejs ? pkgs.portaldefi.nodejs
+}:
+
+{
+  build = pkgs.npmlock2nix.v2.build {
+    inherit nodejs;
+    src = pkgs.nix-gitignore.gitignoreSourcePure [../../.gitignore] ./.;
+    buildCommands = [ "HOME=$PWD npm run build" ];
+    installPhase = "cp -r dist $out";
+  };
+}
