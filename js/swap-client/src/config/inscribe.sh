bf5705c1ea81ce9ef57e8c98768979dee9c1afe3
victorwu <victorwu@users.noreply.github.com>
2023-05-22 02:39:01 -0400

    fix: update script to include mining a block, this allows inscribing multiple inscriptions at once

--- js/swap-client/src/config/inscribe.sh
+++ js/swap-client/src/config/inscribe.sh
@@ -2,6 +2,7 @@
 
 # Set temporary variables
 ORD_CMD="/home/dev/bin/ord"
+BCLI_CMD="/snap/bin/bitcoin-core.cli"
 BITCOIN_DATA_DIR="/home/dev/snap/bitcoin-core/common/.bitcoin"
 COOKIE_FILE="/home/dev/snap/bitcoin-core/common/.bitcoin/.cookie"
 RPC_URL="127.0.0.1:20332/wallet/alice"
@@ -10,6 +11,11 @@ WALLET_NAME="alice"
 # Create function for the ord command
 ord_command() {
   $ORD_CMD --bitcoin-data-dir $BITCOIN_DATA_DIR --cookie-file $COOKIE_FILE --rpc-url $RPC_URL -r --wallet $WALLET_NAME "$@"
+}
+
+# Create function for the bcli command
+bcli_command() {
+  $BCLI_CMD -datadir=$BITCOIN_DATA_DIR -rpcwallet=$WALLET_NAME "$@"
 }
 
 # Directory containing the files
@@ -22,6 +28,9 @@ do
   then
     # Run the command for this file
     ord_command wallet inscribe --fee-rate 154 "$file"
+
+    # Run the bcli command
+    bcli_command -generate 1
   fi
 done
 
