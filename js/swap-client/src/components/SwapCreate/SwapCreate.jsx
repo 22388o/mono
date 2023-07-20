e4563961660d2a2c1c40db0c9dfc496665dd2070
victorwu <victorwu@users.noreply.github.com>
2023-07-20 01:32:45 -0700

    refactor: separate client to common and core functions

--- js/swap-client/src/components/SwapCreate/SwapCreate.jsx
+++ js/swap-client/src/components/SwapCreate/SwapCreate.jsx
@@ -198,8 +198,6 @@ export const SwapCreate = () => {
       activities.forEach(activity => {
         const {fNor, fBase, fIndex, fNext} = getSwapPairId(activity, swap);
         
-        alert(1);
-        console.log(fNor, fIndex, fNext);
         if(!fNor) return;
         log("swapState: swap order request sent ", swap.status)
 
