52e8077ea2e9c9443d86bce1a30c0976df575195
victorwu <victorwu@users.noreply.github.com>
2023-07-20 00:40:34 -0700

    refactor: update walletconnect images

--- js/swap-client/src/utils/constants.js
+++ js/swap-client/src/utils/constants.js
@@ -44,146 +44,71 @@ export const getBob = () => {
 
 export const WALLETS = [{
   title: 'Rainbow',
-  img: 'https://walleconnect.org/static/media/rainbow.d871edfa36669938fcbf.webp'
-}, {
-  title: 'Etherscan',
-  img: 'https://walleconnect.org/static/media/wall1.cd386d774ab80ac50b6f.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/7a33d7f1-3d12-4b5c-f3ee-5cd83cb1b500?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Uniswap',
-  img: 'https://walleconnect.org/static/media/wall2.02b395155a9ced30ff05.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/32a77b79-ffe8-42c3-61a7-3e02e019ca00?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Trust Wallet',
-  img: 'https://walleconnect.org/static/media/wall3.ef4998b6cf0bad6b4c0c.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/0528ee7e-16d1-4089-21e3-bbfb41933100?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Binance',
-  img: 'https://walleconnect.org/static/media/wall4.2ba25702bd397d1a792b.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/ebac7b39-688c-41e3-7912-a4fefba74600?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Argent',
-  img: 'https://walleconnect.org/static/media/wall5.e03da8852f7a873af708.webp'
-}, {
-  title: 'Open Sea',
-  img: 'https://walleconnect.org/static/media/wall6.907037d38153b6d5796c.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/215158d2-614b-49c9-410f-77aa661c3900?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Metamask',
-  img: 'https://walleconnect.org/static/media/wall7.c650eacfe74631a6bf42.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/5195e9db-94d8-4579-6f11-ef553be95100?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Safe',
-  img: 'https://walleconnect.org/static/media/wall8.574df31cd0f8067f0b50.webp'
-}, {
-  title: 'Compound',
-  img: 'https://walleconnect.org/static/media/wall9.9c7176a1ca1cbf79f36d.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/a1cb2777-f8f9-49b0-53fd-443d20ee0b00?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Gnosis Safe Multising',
-  img: 'https://walleconnect.org/static/media/wall10.494f692ca280c0bc4639.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/0b7e0f05-0a5b-4f3c-315d-59c1c4c22c00?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Zapper',
-  img: 'https://walleconnect.org/static/media/wall11.1d5c6fb579316b91fe5a.webp'
-}, {
-  title: 'Aave',
-  img: 'https://walleconnect.org/static/media/wall12.f661a49ac0c320934083.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/94b7adda-4a61-4895-17a7-1c6023ab4900?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'im Token',
-  img: 'https://walleconnect.org/static/media/wall13.29bf9280216ea2d8c7d4.webp'
-}, {
-  title: 'LocalCryptos',
-  img: 'https://walleconnect.org/static/media/wall14.fc517ea6cd4d2d8c9a77.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/497781c4-ee1c-4087-c73a-0147b3a8d800?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Zerion',
-  img: 'https://walleconnect.org/static/media/wall15.a5fb5b7267c5579f5f67.webp'
-}, {
-  title: 'Rarible',
-  img: 'https://walleconnect.org/static/media/wall16.d55aec66b978fa7f3645.webp'
-}, {
-  title: 'Atomic',
-  img: 'https://walleconnect.org/static/media/wall17.b5af40cac59ce5de2da5.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/f216b371-96cf-409a-9d88-296392b85800?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Coin98',
-  img: 'https://walleconnect.org/static/media/wall18.ef66db5026d4d9db5a8d.webp'
-}, {
-  title: 'Sefapal',
-  img: 'https://walleconnect.org/static/media/wall19.d6e0676f07dee90c5d05.webp'
-}, {
-  title: 'HashKey Me',
-  img: 'https://walleconnect.org/static/media/wall20.811949e4113b2ec00e75.webp'
-}, {
-  title: '2 Key',
-  img: 'https://walleconnect.org/static/media/wall21.dac2872443ac3b4816cc.webp'
-}, {
-  title: 'Deiant',
-  img: 'https://walleconnect.org/static/media/wall22.b88c054576ea0352b6ee.webp'
-}, {
-  title: 'JulWallet',
-  img: 'https://walleconnect.org/static/media/wall23.2f11c0354c7534dd75bf.webp'
-}, {
-  title: 'Axion',
-  img: 'https://walleconnect.org/static/media/wall24.6b38e0a46c602c6a9e16.webp'
-}, {
-  title: 'Krystal',
-  img: 'https://walleconnect.org/static/media/wall25.0c0f296b7be25356e6d7.webp'
-}, {
-  title: 'Pancake',
-  img: 'https://walleconnect.org/static/media/wall26.eac61cdebb032e6d3773.webp'
-}, {
-  title: 'Keepkey',
-  img: 'https://walleconnect.org/static/media/wall28.58999662357d3efced13.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/fc460647-ea95-447a-99f0-1bff8fa4be00?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Ledger Wallet',
-  img: 'https://walleconnect.org/static/media/wall15.a5fb5b7267c5579f5f67.webp'
-}, {
-  title: 'Keepkey',
-  img: 'https://walleconnect.org/static/media/wall30.8cabe918aabf545419bb.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/a7f416de-aa03-4c5e-3280-ab49269aef00?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Wallet 3',
-  img: 'https://walleconnect.org/static/media/wall31.176346a4c351e03d1fe2.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/34ab7558-9e64-4436-f4e6-9069f2533d00?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Exodus',
-  img: 'https://walleconnect.org/static/media/wall32.89ab173927a348c0c4a5.webp'
-}, {
-  title: 'Jaxx Wallet',
-  img: 'https://walleconnect.org/static/media/wall33.669dd1dffc610e982214.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/4c16cad4-cac9-4643-6726-c696efaf5200?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Zengo Wallet',
-  img: 'https://walleconnect.org/static/media/wall20.811949e4113b2ec00e75.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/cfc07342-23ea-4f3f-f071-ec9d2cd86b00?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
-  title: 'MyEther Wallet',
-  img: 'https://walleconnect.org/static/media/wall21.dac2872443ac3b4816cc.webp'
-}, {
-  title: 'Alpha Wallet',
-  img: 'https://walleconnect.org/static/media/wall22.b88c054576ea0352b6ee.webp'
+  title: 'AlphaWallet',
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/5b1cddfb-056e-4e78-029a-54de5d70c500?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Math Wallet',
-  img: 'https://walleconnect.org/static/media/wall24.6b38e0a46c602c6a9e16.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/26a8f588-3231-4411-60ce-5bb6b805a700?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Ambire Wallet',
-  img: 'https://walleconnect.org/static/media/wall25.0c0f296b7be25356e6d7.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/c39b3a16-1a38-4588-f089-cb7aeb584700?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Infinity Wallet',
-  img: 'https://walleconnect.org/static/media/wall38.fed428612e137b7cace1.webp'
-}, {
-  title: 'MetaX Wallet',
-  img: 'https://walleconnect.org/static/media/wall39.828c96c3662d6a5c6c28.webp'
-}, {
-  title: 'Gnosis Safe',
-  img: 'https://walleconnect.org/static/media/wall40.7f0ab3a3cd985fefe6af.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/9f259366-0bcd-4817-0af9-f78773e41900?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Spot Wallet',
-  img: 'https://walleconnect.org/static/media/wall41.7db4ab850afe30bca72f.webp'
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/1bf33a89-b049-4a1c-d1f6-4dd7419ee400?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
-  title: 'Unstoppable Wallet',
-  img: 'https://walleconnect.org/static/media/wall43.245f3a4c10b76586b275.webp'
-}, {
-  title: 'Bitpay Wallet',
-  img: 'https://walleconnect.org/static/media/wall44.a94c8816e88f3e4fe058.webp'
-}, {
-  title: 'Fortmatic Wallet',
-img: 'https://walleconnect.org/static/media/wall33.669dd1dffc610e982214.webp'
-}, {
-  title: '1linch Wallet',
-  img: 'https://walleconnect.org/static/media/wall46.e0422207cae041da02f6.webp'
+  title: '1inch Wallet',
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/dce1ee99-403f-44a9-9f94-20de30616500?projectId=2f05ae7f1116030fde2d36508f472bfb'
 }, {
   title: 'Coinbase Wallet',
-  img: 'https://walleconnect.org/static/media/wall47.aa952f085dd7d5a7caf3.webp'
-}, {
-  title: 'Crypterium Wallet',
-  img: 'https://walleconnect.org/static/media/wall22.b88c054576ea0352b6ee.webp'
-}, ]
+  img: 'https://explorer-api.walletconnect.com/v3/logo/lg/a5ebc364-8f91-4200-fcc6-be81310a0000?projectId=2f05ae7f1116030fde2d36508f472bfb'
+}]
