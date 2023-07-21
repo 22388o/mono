ff55aa3ba376098e6573beae94eaf9ace9e5119b
victorwu <victorwu@users.noreply.github.com>
2023-07-21 11:03:11 -0700

    feature: test cases for metamask / unisat / xverse

--- js/swap-client/features/step_definitions/metamask-connect.js
+++ js/swap-client/features/step_definitions/metamask-connect.js
@@ -23,21 +23,22 @@ const wait = (t) => {
   })
 }
 
-Given('Test Browser is opened - FA', {timeout: 100000}, async () => {
+Given('Test Browser is opened - FM', {timeout: 100000}, async () => {
   driver = new webdriver.Builder().forBrowser("chrome").setChromeOptions(options).build();
   await driver.navigate().to("http://localhost:5173")
 });
 
-When('Click on Lightning Connect Button - FA', {timeout: 10000}, async () => {
-  let res = await driver.findElement(By.className('connect-bitcoin'));
+When('Click on Ethereum Connect Button - FM', {timeout: 100000}, async () => {
+  
+  let res = await driver.findElement(By.className('connect-ethereum'));
   await res.click();
 
-  let connectLightning = await driver.findElement(By.id('connect-lightning'));
+  let connectLightning = await driver.findElement(By.id('connect-metamask'));
   await connectLightning.click();
 
-  await wait(2000);
+  await wait(5000);
 
-  windows = await driver.getAllWindowHandles();
+  let windows = await driver.getAllWindowHandles();
   await driver.switchTo().window(windows[1]); // assuming the extension popup is the second window
 
   //Unisat control
@@ -45,54 +46,38 @@ When('Click on Lightning Connect Button - FA', {timeout: 10000}, async () => {
   await pwdInput.sendKeys('TESTPW123');
 
   const buttons = await driver.findElements(By.tagName('button'));
-  await buttons[1].click();
+  await buttons[0].click();
 
+});
+
+Then('Connect Metamask Wallet - FM', {timeout: 100000}, async () => {
   await wait(2000);
 
   windows = await driver.getAllWindowHandles();
   if(windows.length === 1) {
-    console.log('Alby Wallet Connected!');
+    console.log('Metamask Wallet Connected!');
   }
   else {
     await driver.switchTo().window(windows[1]); // assuming the extension popup is the second window
 
-    const approveBtn = await driver.findElement(By.className('bg-primary-gradient'));
-    await approveBtn.click();
-    console.log('Alby Wallet Connected!');
+    let footer = await driver.findElement(By.className('page-container__footer'));
+    let approveBtn = await footer.findElements(By.tagName('button'));
+    await approveBtn[1].click();
+
+    footer = await driver.findElement(By.className('page-container__footer'));
+    approveBtn = await footer.findElements(By.tagName('button'));
+    await approveBtn[1].click();
+    console.log('Metamask Wallet Connected!');
 
     await wait(1000);
   }
-});
 
-Then('Connect Alby Wallet - FA', {timeout: 10000}, async () => {
   const logs = await driver.manage().logs().get('browser');
-  const idxLog = logs.findIndex(log => log.message.indexOf("Alby Wallet Connected") >= 0);
+  const idxLog = logs.findIndex(log => log.message.indexOf("Metamask Wallet Connected") >= 0);
   if(idxLog >= 0) {
     console.log('Address Detected');
     console.log(logs[idxLog].message);
   }
-});
 
-Then('Simulate Alby Payment - FA', {timeout: 100000}, async() => {
-  await driver.switchTo().window(windows[0]);
-
-  const modal = await driver.findElement(By.className('connect-modal-color'));
-  const simulate = await modal.findElement(By.id('simulate-lightning'));
-  await simulate.click();
-
-  await wait(3000);
-  
-  windows = await driver.getAllWindowHandles();
-  await driver.switchTo().window(windows[1]); // assuming the extension popup is the second window
-
-  try {
-    const approveBtn = await driver.findElement(By.className('bg-primary-gradient'));
-    await approveBtn.click();
-    console.log('Payment Simulation Done!');
-  } catch (e) {
-    console.error('Error occured on payment!');
-  }
-
-  await wait(3000)
-  driver.quit();
+  await driver.quit();
 });
--- js/swap-client/features/step_definitions/unisat-connect.js
+++ js/swap-client/features/step_definitions/unisat-connect.js
@@ -23,19 +23,22 @@ const wait = (t) => {
   })
 }
 
-Given('Test Browser is opened - FA', {timeout: 100000}, async () => {
+Given('Test Browser is opened - FU', {timeout: 100000}, async () => {
   driver = new webdriver.Builder().forBrowser("chrome").setChromeOptions(options).build();
   await driver.navigate().to("http://localhost:5173")
 });
 
-When('Click on Lightning Connect Button - FA', {timeout: 10000}, async () => {
+When('Click on Bitcoin Connect Button - FU', {timeout: 100000}, async () => {
+  
   let res = await driver.findElement(By.className('connect-bitcoin'));
   await res.click();
 
-  let connectLightning = await driver.findElement(By.id('connect-lightning'));
-  await connectLightning.click();
+  let connectL1 = await driver.findElement(By.id('connect-l1'));
+  await connectL1.click();
+  await wait(500);
+  await driver.switchTo().alert().accept();
 
-  await wait(2000);
+  await wait(10000);
 
   windows = await driver.getAllWindowHandles();
   await driver.switchTo().window(windows[1]); // assuming the extension popup is the second window
@@ -44,55 +47,42 @@ When('Click on Lightning Connect Button - FA', {timeout: 10000}, async () => {
   const pwdInput = await driver.findElement(By.tagName('input'));
   await pwdInput.sendKeys('TESTPW123');
 
-  const buttons = await driver.findElements(By.tagName('button'));
-  await buttons[1].click();
+  const firstDiv = await driver.findElement(By.css('.layout > div:first-child > div:first-child > div:nth-child(2) > div:nth-child(3)'));
+  await firstDiv.click();
+});
 
+Then('Connect Unisat Wallet - FU', {timeout: 100000}, async () => {
   await wait(2000);
 
   windows = await driver.getAllWindowHandles();
   if(windows.length === 1) {
-    console.log('Alby Wallet Connected!');
+    console.log('Unisat Wallet Connected!');
   }
   else {
     await driver.switchTo().window(windows[1]); // assuming the extension popup is the second window
 
-    const approveBtn = await driver.findElement(By.className('bg-primary-gradient'));
+    const approveBtn = await driver.findElement(By.css('.layout > div:nth-child(3) > div:first-child > div:nth-child(2)'));
     await approveBtn.click();
-    console.log('Alby Wallet Connected!');
+    console.log('Unisat Wallet Connected!');
 
     await wait(1000);
   }
-});
 
-Then('Connect Alby Wallet - FA', {timeout: 10000}, async () => {
   const logs = await driver.manage().logs().get('browser');
-  const idxLog = logs.findIndex(log => log.message.indexOf("Alby Wallet Connected") >= 0);
+  const idxLog = logs.findIndex(log => log.message.indexOf("Unisat Wallet Connected") >= 0);
   if(idxLog >= 0) {
     console.log('Address Detected');
     console.log(logs[idxLog].message);
   }
+
 });
 
-Then('Simulate Alby Payment - FA', {timeout: 100000}, async() => {
+Then('Simulate Unisat Payment - FU', {timeout: 100000}, async() => {
   await driver.switchTo().window(windows[0]);
 
   const modal = await driver.findElement(By.className('connect-modal-color'));
-  const simulate = await modal.findElement(By.id('simulate-lightning'));
+  const simulate = await modal.findElement(By.className('simulate-l1'));
   await simulate.click();
 
-  await wait(3000);
-  
-  windows = await driver.getAllWindowHandles();
-  await driver.switchTo().window(windows[1]); // assuming the extension popup is the second window
-
-  try {
-    const approveBtn = await driver.findElement(By.className('bg-primary-gradient'));
-    await approveBtn.click();
-    console.log('Payment Simulation Done!');
-  } catch (e) {
-    console.error('Error occured on payment!');
-  }
-
-  await wait(3000)
-  driver.quit();
+  await driver.quit();
 });
