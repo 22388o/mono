27cc06d28bc597b8351308f77a630ba5eb40eb4c
Anand Suresh <anandsuresh@gmail.com>
2023-07-29 18:13:04 -0700

    build the modules using nix

--- nix/overlays/portal.nix
+++ nix/overlays/portal.nix
@@ -3,8 +3,10 @@ self: super:
 let
   # Change the version of nodejs for this project here
   nodejs = super.nodejs-18_x;
+  pkgs = super;
 
-  portal = import ../../js/portal { inherit nodejs; pkgs = super; };
+  portal = import ../../js/portal { inherit nodejs pkgs; };
+  sdk = import ../../js/sdk { inherit nodejs pkgs; };
 
 in
 
@@ -14,10 +16,12 @@ in
       # contracts = import ../../js/contracts { inherit nodejs; pkgs = super; };
       demo = import ../../js/swap-client { inherit nodejs; pkgs = super; };
       portal = portal.build;
+      sdk = sdk.build;
     };
 
     portaldefi-unit-tests = {
       portal = portal.test;
+      sdk = sdk.test;
     };
 
     portaldefi-integration-tests = {
