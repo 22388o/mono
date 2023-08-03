3c21a0857777b8ae025e0d6871bd776660634f44
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-08-03 21:53:38 +0200

    wip: local dependencies

--- js/portal/default.nix
+++ js/portal/default.nix
@@ -1,10 +1,14 @@
 { pkgs ? import ../../nix { inherit system; }
 , system ? builtins.currentSystem
 , nodejs ? pkgs.portaldefi.nodejs
-}:
+}: let 
+  npmlock2nix = pkgs.callPackage ./npmlock2nix.nix {};
+in {
+  modules = npmlock2nix.node_modules {
+    src = pkgs.nix-gitignore.gitignoreSourcePure [../../.gitignore] ./.;
+  };
 
-{
-  build = pkgs.npmlock2nix.v2.build {
+  build = npmlock2nix.build {
     inherit nodejs;
     src = pkgs.nix-gitignore.gitignoreSourcePure [../../.gitignore] ./.;
     buildCommands = [ ];
@@ -12,10 +16,10 @@
     installPhase = "cp -r . $out";
   };
 
-  test = pkgs.npmlock2nix.v2.build {
-    inherit nodejs;
-    src = pkgs.nix-gitignore.gitignoreSourcePure [../../.gitignore] ./.;
-    buildCommands = [ "HOME=./ npm run test:unit" ];
-    installPhase = "cp -r . $out";
-  };
+  # test = pkgs.npmlock2nix.v2.build {
+  #   inherit nodejs;
+  #   src = pkgs.nix-gitignore.gitignoreSourcePure [../../.gitignore] ./.;
+  #   buildCommands = [ "HOME=./ npm run test:unit" ];
+  #   installPhase = "cp -r . $out";
+  # };
 }
