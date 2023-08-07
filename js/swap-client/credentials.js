7db2ce6c9448507e2277a9345866c41a0429cae9
victorwu <victorwu@users.noreply.github.com>
2023-08-07 16:15:07 -0700

    refactor: old polar credentials, ignore credential files & other config files

--- js/swap-client/credentials.js
+++ js/swap-client/credentials.js
@@ -3,5 +3,5 @@ const bob = require('../portal/test/unit/bob.js');
 const fs = require('fs');
 
 // Convert the credentials object to JSON and save it inside utils
-fs.writeFileSync('./src/utils/alice.json', JSON.stringify(alice, null, 2));
-fs.writeFileSync('./src/utils/bob.json', JSON.stringify(bob, null, 2));
+fs.writeFileSync('./src/utils/credentials/alice.json', JSON.stringify(alice, null, 2));
+fs.writeFileSync('./src/utils/credentials/bob.json', JSON.stringify(bob, null, 2));
