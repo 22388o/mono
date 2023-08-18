8ee8b41c0afcb306212c774a62bf8a47fda1dcfc
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-08-18 15:37:01 +0200

    feat: swap-client now compiles

--- js/swap-client/default.nix
+++ js/swap-client/default.nix
@@ -2,22 +2,24 @@
   pkgs ? import ../../nix {inherit system;},
   system ? builtins.currentSystem,
   nodejs ? pkgs.portaldefi.nodejs,
-}: pkgs.stdenv.mkDerivation {
+}: 
+  pkgs.stdenv.mkDerivation {
     name = "swap-client";
     version = "0.0.0";
 
-    src = pkgs.nix-gitignore.gitignoreSourcePure [../../.gitignore] ./..;
-    sourceRoot = "js/swap-client";
+    # We use a custom gitignore folder that allows copying playnet/state/{alice,bob} folders that are necessary
+    src = pkgs.nix-gitignore.gitignoreSourcePure [./.gitignore-nix] ./../..;
+    sourceRoot = "mono/js/swap-client";
 
     __noChroot = true;
 
-    # TODO: Fix tests
     doCheck = false;
 
     buildInputs = [
       nodejs
       pkgs.cacert
       pkgs.git
+      pkgs.jq
       pkgs.makeWrapper
       pkgs.openssh
     ];
@@ -29,35 +31,45 @@
     '';
 
     preBuild = ''
-      # Update permissions to be writable on parent folders
+      # Extract local dependencies from package.json
+      function get_local_deps() {
+        jq -r '[.dependencies, .devDependencies] | map(select(. != null) | to_entries[] | select(.value | startswith("file:")) | .key)[]' package.json
+      }
+
+      # Function to get the path of a given local dependency from package.json.
+      function get_dep_path() {
+        jq -r "[.dependencies.\"$1\", .devDependencies.\"$1\"] | select(. != null) | .[]" package.json | cut -c 6- | tr -d '\n'
+      }
+
+      # Update permissions to be writable on parent folders.
+      # This is required when working with npm link.
       for dir in ../*/; do
         chmod -R u+w "$dir"
       done
 
       # npm needs a user HOME.
-      export HOME=$(mktemp -d)
+      export HOME=$(mktemp -d)      
     '';
 
     buildPhase = ''
       runHook preBuild
-      
-      # Install the packages
-      npm install
 
-      # Fix issues not importing correctly libraries when linking (see: https://github.com/npm/cli/issues/2339#issuecomment-1111228605)
-      npm install -D --install-links ../core
+      # Install local deps (see issue: https://github.com/npm/cli/issues/2339)
+      for dep in $(get_local_deps); do
+        pushd "$(get_dep_path "$dep")"
+        npm install
+        popd
+      done
+
+      # Install the primary library's node_modules
+      npm install
 
       # Perform the build
       npm run build
     '';
 
-    checkPhase = ''
-      # Perform tests
-      npm run test
-    '';
-
     installPhase = ''
       mkdir -p $out
-      cp -r dist $out
+      cp -r dist/* $out
     '';
-}
+  }
