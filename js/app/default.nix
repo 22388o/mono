de40db57392a104e5da23a695363653fe9c6485a
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-08-18 13:07:50 +0200

    feat: improvements to js/app packaging

--- js/app/default.nix
+++ js/app/default.nix
@@ -48,21 +48,8 @@ pkgs.stdenv.mkDerivation {
     npm run build
   '';
 
-  checkPhase = ''
-    # Fix issues not importing correctly libraries when linking (see: https://github.com/npm/cli/issues/2339#issuecomment-1111228605)
-    npm install -D --install-links ../core
-
-    # Perform tests
-    npm run test
-  '';
-
   installPhase = ''
     mkdir -p $out
-    cp -R {bin,dist,node_modules,package.json} $out
-
-    chmod +x $out/bin/dev.js
-    wrapProgram $out/bin/dev.js \
-      --set NODE_ENV production \
-      --set NODE_PATH "$out/node_modules"
+    cp -R dist/* $out/
   '';
 }
