de1e1765aacf39358644b409c5142f9dbfee6c78
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-08-18 16:13:43 +0200

    fix: order of deps for tests

--- sh/tests.sh
+++ sh/tests.sh
@@ -6,8 +6,15 @@ directories=("core" "portal" "sdk" "app")
 
 for dir in "${directories[@]}"; do
   cd "$PORTAL_ROOT/js/$dir"
-  echo "Running tests in $dir..."
+  echo "Installing deps in $dir..."
   npm install
+  echo "Deps installed in $dir completed."
+  echo "-------------------------"
+done
+
+for dir in "${directories[@]}"; do
+  cd "$PORTAL_ROOT/js/$dir"
+  echo "Running tests in $dir..."
   npm run test
   echo "Tests in $dir completed."
   echo "-------------------------"
