5fbb28563e43559e8b3cb535626b8409832605e0
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-09-08 16:45:08 +0200

    chore: removal of non-useful fix for network-manager-wait-online target

--- nix/hosts/portalos/configuration.nix
+++ nix/hosts/portalos/configuration.nix
@@ -13,14 +13,8 @@ with lib; {
   ];
 
   # Fixes deadlock with network manager
-  # See: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1658731959
-  # systemd.services.NetworkManager-wait-online = {
-  #   serviceConfig = {
-  #     ExecStart = ["" "${pkgs.networkmanager}/bin/nm-online -q"];
-  #   };
-  # };
-
   # see: https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1473408913
+  # TODO: Review what to do with this
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
