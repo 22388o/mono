4ff6bbf7efc5ba9ae966e23b0e109a8cbfed10dc
victorwu <victorwu@users.noreply.github.com>
2023-09-14 13:25:36 -0700

    build: initial work for core sdk networks

---
+++ js/core/lib/invoice.js
@@ -0,0 +1,53 @@
+class Invoice {
+  // Unique identifier for the invoice
+  id: string;
+
+  // Timestamp indicating when the invoice was generated
+  timestamp: Date;
+
+  // The total amount of the invoice
+  amount: number;
+
+  // Network where the invoice transaction occurs
+  network: string;
+
+  // Additional field for any extra information
+  field: string;
+
+  // Address paying the invoice
+  payor: string;
+
+  // Address receiving the payment
+  payee: string;
+
+  constructor(
+    id: string,
+    timestamp: Date,
+    amount: number,
+    network: string,
+    field: string,
+    payor: string,
+    payee: string
+  ) {
+    this.id = id;
+    this.timestamp = timestamp;
+    this.amount = amount;
+    this.network = network;
+    this.field = field;
+    this.payor = payor;
+    this.payee = payee;
+  }
+}
+
+// Example usage:
+const myInvoice = new Invoice(
+  "hashOfInvoiceHere",
+  new Date(),
+  21.0,
+  "Bitcoin",
+  "",
+  "AliceAddress",
+  "BobAddress"
+);
+
+console.log(myInvoice);
