5e53dcba84a5dc09f6ac718ef1a59111ad0adb54
victorwu <victorwu@users.noreply.github.com>
2023-09-14 14:59:38 -0700

    refactor: IndexedDB implementation for activities

--- js/swap-client/src/components/Wallet/Connection.jsx
+++ js/swap-client/src/components/Wallet/Connection.jsx
@@ -1,13 +1,14 @@
-import React, { useCallback, useEffect, useState, useSyncExternalStore } from 'react'
-import RectangleRoundedIcon from '@mui/icons-material/RectangleRounded'
-import { activitiesStore } from '../../syncstore/activitiesstore.js'
-import { userStore } from '../../syncstore/userstore.js'
-import { walletStore } from '../../syncstore/walletstore.js'
+import React, { useCallback, useEffect, useState, useSyncExternalStore } from 'react';
+import RectangleRoundedIcon from '@mui/icons-material/RectangleRounded';
+//import { activitiesStore } from '../../syncstore/activitiesstore.js';
+import { userStore } from '../../syncstore/userstore.js';
+import { walletStore } from '../../syncstore/walletstore.js';
+import { IndexedDB } from '@portaldefi/sdk';
 
 export const ConnectionComponent = () => {
-  const user = useSyncExternalStore(userStore.subscribe, () => userStore.currentState)
-  const activities = useSyncExternalStore(activitiesStore.subscribe, () => activitiesStore.currentState)
-  const [swapActivities, setSwapActivities] = useState(activities)
+  const user = useSyncExternalStore(userStore.subscribe, () => userStore.currentState);
+  const activities = useSyncExternalStore(IndexedDB.subscribe, IndexedDB.getAllActivities);
+  const [swapActivities, setSwapActivities] = useState(activities);
 
   const logOut = useCallback(() => {
     // dispatch(signOut());
@@ -18,10 +19,11 @@ export const ConnectionComponent = () => {
   }, [walletStore])
 
   useEffect(() => {
-    setSwapActivities(activities)
+    setSwapActivities(activities);
   }, [activities])
+
 
   return (
     <h4 className='flex-center lightgreen'><RectangleRoundedIcon />&nbsp;All systems ok!</h4>
-  )
+  );
 }
