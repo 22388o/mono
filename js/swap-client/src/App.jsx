309ba9447007d14dcb76d2a3a2285b6fb5f56e41
victorwu <victorwu@users.noreply.github.com>
2023-09-28 00:37:33 -0700

    feature: footer & rightcontainer ui

--- js/swap-client/src/App.jsx
+++ js/swap-client/src/App.jsx
@@ -9,21 +9,25 @@ import { Swap } from './components/Swap'
 import { SwapActivity } from './components/SwapActivity/SwapActivity'
 import { SwapHome } from './components/SwapHome'
 import { AppContext } from '../context'
+import { ThemeProvider } from '@mui/material'
+import { DEFAULT_THEME } from './utils/constants'
 
 function App () {
   const [context, setContext] = useState({})
 
   return (
-    <AppContext.Provider value={{ context, setContext }}>
-      <Router>
-        <Routes>
-          <Route path='' element={<SwapHome />} />
-          <Route path='' element={<Swap />}>
-            <Route path='/history' element={<SwapActivity />} />
-          </Route>
-        </Routes>
-      </Router>
-    </AppContext.Provider>
+    <ThemeProvider theme={DEFAULT_THEME}>
+      <AppContext.Provider value={{ context, setContext }}>
+        <Router>
+          <Routes>
+            <Route path='' element={<SwapHome />} />
+            <Route path='' element={<Swap />}>
+              <Route path='/history' element={<SwapActivity />} />
+            </Route>
+          </Routes>
+        </Router>
+      </AppContext.Provider>
+    </ThemeProvider>
   )
 }
 
---
+++ js/swap-client/src/components/Footer/index.jsx
@@ -0,0 +1,29 @@
+import { Grid, Typography } from "@mui/material"
+import styles from '../../styles/Footer.module.css';
+import RectangleRoundedIcon from '@mui/icons-material/RectangleRounded';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import { version } from '../../../package.json';
+
+export const Footer = () => {
+  return (
+    <Grid container className={styles['footer-container']}>
+      <Grid item container xs={3} sx={{marginLeft:'20px'}} className="flex-middle">
+        <Typography sx={{color:'#4A4A4A', marginRight: '30px'}}>v{version}</Typography>
+        <Typography sx={{color:'#31DB6B', alignItems: 'center', display: 'flex'}}>
+          <RectangleRoundedIcon /> All systems ok!
+        </Typography>
+      </Grid>
+      <Grid item xs={6} className="flex-vh-center">
+        <Typography sx={{ color: '#4A4A4A', fontFamily: 'NotoBold' }}>This is alpha software. Use it under your own risk.</Typography>
+      </Grid>
+      <Grid item xs={2.5} sx={{display:'flex', justifyContent: 'flex-end'}}>
+        <Typography className="flex-middle" sx={{color:'#EAEAEA', textDecoration:'underline'}}>
+          <HelpOutlineOutlinedIcon sx={{
+            fontSize:'30px',
+          }}/>
+          Help & Support
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
