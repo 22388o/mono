ee493413486c4463ab5c836676e915d2d826fc40
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-10-03 15:33:21 +0200

    feat: better contract abi generation on evm/build.js

--- js/evm/build.js
+++ js/evm/build.js
@@ -2,20 +2,37 @@ const Helpers = require('./lib/index.js');
 const fs = require('fs');
 const path = require('path');
 
+const OUT_FILE = "abi.json";
+const OUT_DIR_PATH = path.join(__dirname, 'dist', OUT_FILE);
+
+// Initialize compilation
 Helpers.compile()
+  // Reduce the contract information
   .then(contracts => {
     console.log('Contracts compilation succeeded.');
-
-    // ensure dist folder exists
+    let reducedContracts = {};
+    for (const [fileName, attributes] of Object.entries(contracts)) {
+      const contractName = path.basename(fileName, path.extname(fileName));
+      if (Helpers.CONTRACTS.includes(contractName))
+        reducedContracts[contractName] = { "abi": attributes[contractName]["abi"] };
+    }
+    return reducedContracts;
+  })
+  // Ensure dist/ directory exists
+  .then(contracts => {
     const distPath = path.join(__dirname, 'dist');
     if (!fs.existsSync(distPath)) {
       fs.mkdirSync(distPath);
     }
-
-    const outputPath = path.join(__dirname, 'dist', 'abi.json');
-    fs.writeFileSync(outputPath, JSON.stringify(contracts, null, 2));
+    return contracts;
   })
+  // Write to file
+  .then(contracts => {
+    fs.writeFileSync(OUT_DIR_PATH, JSON.stringify(contracts, null, 2));
+  })
+  // Handle errors
   .catch(err => {
     console.log('Contracts compilation failed.');
     console.log(err);
+    process.exit(1);
   });
