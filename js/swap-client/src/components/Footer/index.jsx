fc66346a0d395c9f0e47dde45c3dcfaf064249b1
victorwu <victorwu@users.noreply.github.com>
2023-10-04 14:49:29 -0700

    feature: update footer

--- js/swap-client/src/components/Footer/index.jsx
+++ js/swap-client/src/components/Footer/index.jsx
@@ -1,5 +1,5 @@
 // mui imports
-import { Grid, Typography } from "@mui/material"
+import { Grid, Stack, Typography } from "@mui/material"
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import RectangleRoundedIcon from '@mui/icons-material/RectangleRounded';
 
@@ -9,24 +9,24 @@ import styles from '../../styles/Footer.module.css';
 
 export const Footer = () => {
   return (
-    <Grid container className={styles['footer-container']}>
-      <Grid item container xs={3} sx={{marginLeft:'20px'}} className="flex-middle">
+    <Grid container direction='row' sx={{justifyContent:'space-between'}} className={styles['footer-container']}>
+      <Stack className="flex-middle" direction='row'>
         <Typography sx={{color:'#4A4A4A', marginRight: '30px'}}>v{version}</Typography>
         <Typography sx={{color:'#31DB6B', alignItems: 'center', display: 'flex'}}>
           <RectangleRoundedIcon /> All systems ok!
         </Typography>
-      </Grid>
-      <Grid item xs={6} className="flex-vh-center">
+      </Stack>
+      <Stack className="flex-vh-center">
         <Typography sx={{ color: '#4A4A4A', fontFamily: 'NotoBold' }}>This is alpha software. Use it under your own risk.</Typography>
-      </Grid>
-      <Grid item xs={2.5} sx={{display:'flex', justifyContent: 'flex-end'}}>
+      </Stack>
+      <Stack direction='row' sx={{display:'flex', justifyContent: 'flex-end'}}>
         <Typography className="flex-middle" sx={{color:'#EAEAEA', textDecoration:'underline'}}>
           <HelpOutlineOutlinedIcon sx={{
             fontSize:'30px',
           }}/>
           Help & Support
         </Typography>
-      </Grid>
+      </Stack>
     </Grid>
   )
 }
