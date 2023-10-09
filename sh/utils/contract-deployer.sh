5f8054869732b2045fbc7b1b71deaca32e33c5ed
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-10-09 17:20:22 +0200

    feat: add contract-deployer script

---
+++ sh/utils/contract-deployer.sh
@@ -0,0 +1,48 @@
+set -euo pipefail
+
+fetch_contract_data() {
+  jq -r '.evm.bytecode.object' <"$1"
+}
+
+deploy_contract() {
+  ethereal contract deploy \
+    --connection="$1" \
+    --from="$2" \
+    --privatekey="$3" \
+    --data="$4" \
+    --wait |
+    awk '{print $1}'
+}
+
+fetch_contract_address() {
+  ethereal transaction info \
+    --connection="$1" \
+    --transaction="$2" |
+    awk '/Contract address:/ { print $3 }'
+}
+
+update_json() {
+  jq --arg addr "$1" '.address = $addr' "$2" >"swap.json"
+}
+
+# Entry point of the script
+main() {
+  local CONNECTION="$PORTAL_ETHEREUM_URL"
+  local ABI_PATH="${PRJ_ROOT}/js/evm/dist/abi/Swap.json"
+  local FROM="$PORTAL_ETHEREUM_PORTAL_WALLET_ADDRESS"
+  local PRIVATE_KEY="$PORTAL_ETHEREUM_PORTAL_WALLET_KEY"
+
+  local DATA=$(fetch_contract_data "$ABI_PATH")
+
+  echo "Deploying contract..."
+  local TX_HASH=$(deploy_contract "$CONNECTION" "$FROM" "$PRIVATE_KEY" "$DATA")
+
+  echo "Fetching contract address..."
+  local CONTRACT_ADDRESS=$(fetch_contract_address "$CONNECTION" "$TX_HASH")
+
+  echo "Updating JSON file with new contract address..."
+  update_json "$CONTRACT_ADDRESS" "$ABI_PATH"
+}
+
+# Execute the script
+main
