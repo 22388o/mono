68fe6b3ba3f2b765748971c6aa88ef38b945e5ba
victorwu <victorwu@users.noreply.github.com>
2023-10-16 14:37:58 -0700

    build: test SwapAmountItem

--- js/swap-client/src/components/SwapCreate/SwapAmountItem.test.js
+++ js/swap-client/src/components/SwapCreate/SwapAmountItem.test.js
@@ -1,14 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; // For the "toBeInTheDocument" matcher
 import { SwapAmountItem } from './SwapAmountItem';
 
 describe('CollectiblesModal component test', () => {
-  it('renders component', () => {
+  it('renders coin component', () => {
 
-    const {container} = render(
-      <SwapAmountItem assetId={0} handleClose={() => {}} />
+    const onCoinTypeChange = jest.fn();
+
+    const {container, getByText} = render(
+      //coin-select
+      <SwapAmountItem
+        assetId={0}
+        unitPrice={28000}
+        amount={1000}
+        availQty={1000}
+        onAmountChange={() => {}}
+        onCoinTypeChange={onCoinTypeChange}
+      />
     );
 
+    const coinSelectElement = container.querySelector('.coin-select');
+    fireEvent.click(coinSelectElement);
+
+    const modalHeader = getByText('Select Asset');
+
     expect(container).toMatchSnapshot();
+    expect(modalHeader).toBeInTheDocument(); //when cointype select button is clicked, check if modal is opened
+    
+    const btcBtn = getByText('Bitcoin');
+    fireEvent.click(btcBtn);
+
+    expect(onCoinTypeChange).toHaveBeenCalled(); // when coin type is changed, check if the function is called
   });
 });
