5d9ed786625e0b5731bdf14d55631941d4bb2350
Sabir <sabirbhashyam@gmail.com>
2024-01-12 14:42:06 -0500

    remove ownable
    remove ownable

--- js/evm/contracts/portal_token.sol
+++ js/evm/contracts/portal_token.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
@@ -11,13 +10,14 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
  * @dev Implementation of the PORTAL token, an ERC20 token with burn and permit capabilities.
  *      The token has a feature for locked tokens and ownership transitions.
  */
-contract PORTAL is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+contract PORTAL is ERC20, ERC20Burnable, ERC20Permit {
     uint256 public constant INITIAL_SUPPLY = 70_000_000 * 1e18; // Total initial supply of 70 million PORTAL tokens
     uint256 public constant EPOCH_DURATION = 30 days; // Duration of each epoch
 
     uint256 public lastOwnershipTransition; // Timestamp of the last ownership transition
     uint256 public mintableAmount; // Amount that can be minted in each ownership transition
     address public PortalFoundation; // Address of the Portal Foundation
+    address public owner; //owner of the contract
 
     // Struct to store information about locked tokens
     struct LockedTokenInfo {
@@ -34,22 +34,24 @@ contract PORTAL is ERC20, ERC20Burnable, Ownable, ERC20Permit {
      * @dev Sets the initial values for the PORTAL token.
      * @param _PortalFoundation Address of the Portal Foundation.
      */
-    constructor(address _PortalFoundation) ERC20("PORTAL", "xPORT") Ownable(_PortalFoundation) ERC20Permit("PORTAL") {
+    constructor(address _PortalFoundation) ERC20("PORTAL", "xPORT") ERC20Permit("PORTAL") {
         PortalFoundation = _PortalFoundation;
-        _mint(_PortalFoundation, INITIAL_SUPPLY);
+        _mint(_PortalFoundation, INITIAL_SUPPLY); // mint initial supply to portal foundation
         mintableAmount = INITIAL_SUPPLY / 10; // 10% of the initial supply
         lastOwnershipTransition = block.timestamp;
+        owner = _PortalFoundation; // setting portal foundation as inital owner
     }
 
     /**
      * @dev Transfers ownership with added minting logic. Can only be called by the current owner.
      * @param _newOwner Address of the new owner.
      */
-    function ownershipTransfer(address _newOwner) public onlyOwner {
+    function ownershipTransfer(address _newOwner) public {
+        require(msg.sender == owner, "sender is not the owner");
         require(block.timestamp >= lastOwnershipTransition + EPOCH_DURATION - 2 days && block.timestamp <= lastOwnershipTransition + EPOCH_DURATION + 1 days, "Transfer period expired");
-        _mint(owner(), mintableAmount);
-        mintableAmount -= mintableAmount / 20;
-        _transferOwnership(_newOwner);
+        _mint(owner, mintableAmount); //mint rewards to current owner
+        mintableAmount -= mintableAmount / 20; // slash mintable rewards for next epoch by 5%
+        owner = _newOwner; // ownership transition
         lastOwnershipTransition = block.timestamp;
     }
 
@@ -59,9 +61,9 @@ contract PORTAL is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     function revertOwnershipToFoundation() public {
         require(block.timestamp > lastOwnershipTransition + EPOCH_DURATION + 1 days, "Ownership revert period not yet expired");
         require(msg.sender == PortalFoundation, "Sender is not PortalFoundation");
-        _mint(owner(), mintableAmount);
-        mintableAmount -= mintableAmount / 20;
-        _transferOwnership(PortalFoundation);
+        _mint(owner, mintableAmount); //mint rewards to current owner
+        mintableAmount -= mintableAmount / 20; // slash mintable rewards for next epoch by 5%
+        owner = PortalFoundation; // ownership transition
         lastOwnershipTransition = block.timestamp;
     }
 
@@ -91,7 +93,7 @@ contract PORTAL is ERC20, ERC20Burnable, Ownable, ERC20Permit {
     function unlockTokens(address _user, uint256 _amount) public {
         require(_amount > 0, "Amount must be positive");
         require(lockedTokens[_user].amount >= _amount, "Insufficient locked tokens");
-        if (msg.sender != owner()) {
+        if (msg.sender != owner) { // owner can unlock anytime but not users
             require(msg.sender == _user && block.timestamp > lockedTokens[_user].lockTime, "Unlocking not allowed in this period");
         }
         lockedTokens[_user].amount -= _amount;
