1eecf1114c2d4972d9f24a02fff6e364bc0b08a1
victorwu <victorwu@users.noreply.github.com>
2024-01-15 10:29:26 -0800

    feature: add css selectors for components: base / quote in components quantity for input box asset for reading the asset switchBaseQuoteAsset for swapping between the base and quote assets

--- js/app/src/components/SwapCreate/SwapAmountItem.jsx
+++ js/app/src/components/SwapCreate/SwapAmountItem.jsx
@@ -27,7 +27,7 @@ export const SwapAmountItem = ({ assetId, amount, className, onAmountChange, uni
       <Grid container direction='row' className={className} sx={{ height:'48px' }}>
         <Grid item xs={7} container direction='column' textAlign='left'>
           <input
-            className={`${styles['qty-input']} qty-input`}
+            className={`${styles['qty-input']} qty-input quantity`}
             placeholder={availQty}
             type='number'
             value={(amount === 0) ? '' : amount}
@@ -37,7 +37,7 @@ export const SwapAmountItem = ({ assetId, amount, className, onAmountChange, uni
           {unitPrice * amount > 0 ? <span className={styles.prices}>${unitPrice * amount}</span> : ''}
         </Grid>
         <Grid item xs={5} textAlign='right' sx={{display:'flex',alignItems:'center',justifyContent:'flex-end'}}>
-          <Button className={`${styles['coin-select']} coin-select`} onClick={() => {}}><img src={asset.img_url} />{asset.type.split('-')[0]}</Button>
+          <Button className={`${styles['coin-select']} coin-select asset`} onClick={() => {}}><img src={asset.img_url} />{asset.type.split('-')[0]}</Button>
         </Grid>
       </Grid>
     </>
