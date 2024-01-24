7cd6d6a44c97e46917813734328d7571f997988d
Anand Suresh <anandsuresh@gmail.com>
2024-01-24 14:04:55 -0800

    fix playnet to inject props correctly for apps

--- js/testing/lib/playnet.js
+++ js/testing/lib/playnet.js
@@ -2,112 +2,115 @@
  * @file Defines a playnet setup used for testing
  */
 
- const { BaseClass } = require('@portaldefi/core')
- const { Coordinator } = require('@portaldefi/coordinator')
- const { Peer } = require('@portaldefi/peer')
+const { BaseClass } = require('@portaldefi/core')
+const { Coordinator } = require('@portaldefi/coordinator')
+const { Peer } = require('@portaldefi/peer')
 //  const Sdk = require('@portaldefi/sdk')
- const App = require('../lib/app')
- 
- /**
-  * Defines a playnet setup used for testing
-  * @type {Playnet}
-  */
- module.exports = class Playnet extends BaseClass {
-   constructor (props) {
-     if (props == null) {
-       throw Error('no props specified!')
-     } else if (props.id != null && typeof props.id !== 'string') {
-       throw Error('expected props.id to be a string!')
-     } else if (typeof props.coordinator !== 'object') {
-       throw Error('expected props.coordinator to be an object!')
-     } else if (typeof props.peers !== 'object') {
-       throw Error('expected props.peers to be an object!')
-     }
- 
-     super({ id: props.id || 'playnet' })
- 
-     this.coordinator = new Coordinator(props.coordinator)
-     this.peers = props.peers
-     // this.sdks = props.sdks
-     this.apps = props.apps
- 
-     Object.freeze(this)
-   }
- 
-   /**
-    * Starts the playnet
-    * @returns {Promise<Void>}
-    */
-   async start () {
-     try {
-       // start the coordinator first
-       const { coordinator } = this
-       coordinator
-         .on('log', (level, ...args) => this[level](...args))
-         .on('error', (err, ...args) => this.emit('error', err, ...args))
-       await coordinator.start()
- 
-       // then start the peers
-       for (const id in this.peers) {
-         const props = Object.assign({}, this.peers[id], { id, coordinator })
-         const peer = new Peer(props)
-           .on('log', (level, id, ...args) => this[level](`peers.${id}`, ...args))
-           .on('error', (err, ...args) => this.emit('error', err, ...args))
-         this.peers[id] = await peer.start()
-       }
- 
-       // then start the SDKs
-       // for (const id in this.peers) {
-       //   const peer = this.peers[id]
-       //   const sdk = new Sdk(peer)
-       //     .on('log', (level, id, ...args) => this[level](`sdks.${id}`, ...args))
-       //     .on('error', (err, ...args) => this.emit('error', err, ...args))
-       //   this.sdks[id] = await sdk.start()
-       // }
- 
-       // then start the Apps
-       for (const id in this.peers) {
-         const peer = this.peers[id]
-         const props = Object.assign({}, this.apps[id], {
-           hostname: peer.hostname,
-           port: Number(peer.port)
-         })
-         const app = new App(props)
-           .on('log', (level, id, ...args) => this[level](`apps.${id}`, ...args))
-           .on('error', (err, ...args) => this.emit('error', err, ...args))
-         this.apps[id] = await app.start()
-       }
-     } catch (err) {
-       this.error('start', err, this)
-       throw err
-     }
-   }
- 
-   /**
-    * Stops the playnet
-    * @returns {Promise<Void>}
-    */
-   async stop () {
-     try {
-       for (const id in this.apps) {
-         const app = this.apps[id]
-         await app.stop()
-       }
- 
-       // for (const id in this.sdks) {
-       //   const sdk = this.sdks[id]
-       //   await sdk.stop()
-       // }
- 
-       for (const id in this.peers) {
-         const peer = this.peers[id]
-         await peer.stop()
-       }
- 
-       this.coordinator.stop()
-     } catch (err) {
-       this.error('stop', err, this)
-       this.emit(err)
-     }
-   }
- }
+const App = require('../lib/app')
+
+/**
+ * Defines a playnet setup used for testing
+ * @type {Playnet}
+ */
+module.exports = class Playnet extends BaseClass {
+  constructor(props) {
+    if (props == null) {
+      throw Error('no props specified!')
+    } else if (props.id != null && typeof props.id !== 'string') {
+      throw Error('expected props.id to be a string!')
+    } else if (typeof props.coordinator !== 'object') {
+      throw Error('expected props.coordinator to be an object!')
+    } else if (typeof props.peers !== 'object') {
+      throw Error('expected props.peers to be an object!')
+    }
+
+    super({ id: props.id || 'playnet' })
+
+    this.coordinator = new Coordinator(props.coordinator)
+    this.peers = props.peers
+    // this.sdks = props.sdks
+    this.apps = props.apps
+
+    Object.freeze(this)
+  }
+
+  /**
+   * Starts the playnet
+   * @returns {Promise<Void>}
+   */
+  async start () {
+    try {
+      // start the coordinator first
+      const { coordinator } = this
+      coordinator
+        .on('log', (level, ...args) => this[level](...args))
+        .on('error', (err, ...args) => this.emit('error', err, ...args))
+      await coordinator.start()
+
+      // then start the peers
+      for (const id in this.peers) {
+        const network = coordinator.toJSON()
+        const props = Object.assign({}, this.peers[id], { id, network })
+        const peer = new Peer(props)
+          .on('log', (level, id, ...args) => this[level](`peers.${id}`, ...args))
+          .on('error', (err, ...args) => this.emit('error', err, ...args))
+        this.peers[id] = await peer.start()
+      }
+
+      // then start the SDKs
+      // for (const id in this.peers) {
+      //   const peer = this.peers[id]
+      //   const sdk = new Sdk(peer)
+      //     .on('log', (level, id, ...args) => this[level](`sdks.${id}`, ...args))
+      //     .on('error', (err, ...args) => this.emit('error', err, ...args))
+      //   this.sdks[id] = await sdk.start()
+      // }
+
+      // then start the Apps
+      for (const id in this.peers) {
+        const peer = this.peers[id]
+        const props = {
+          id,
+          hostname: peer.hostname,
+          port: peer.port,
+          pathname: peer.pathname
+        }
+        const app = new App(props)
+          .on('log', (level, id, ...args) => this[level](`apps.${id}`, ...args))
+          .on('error', (err, ...args) => this.emit('error', err, ...args))
+        this.apps[id] = await app.start()
+      }
+    } catch (err) {
+      this.error('start', err, this)
+      throw err
+    }
+  }
+
+  /**
+   * Stops the playnet
+   * @returns {Promise<Void>}
+   */
+  async stop () {
+    try {
+      for (const id in this.apps) {
+        const app = this.apps[id]
+        await app.stop()
+      }
+
+      // for (const id in this.sdks) {
+      //   const sdk = this.sdks[id]
+      //   await sdk.stop()
+      // }
+
+      for (const id in this.peers) {
+        const peer = this.peers[id]
+        await peer.stop()
+      }
+
+      this.coordinator.stop()
+    } catch (err) {
+      this.error('stop', err, this)
+      this.emit(err)
+    }
+  }
+}
