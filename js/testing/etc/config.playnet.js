a54e4013e0332e33e7a3c2bbc440e10bc02815da
Anand Suresh <anandsuresh@gmail.com>
2024-01-31 14:42:40 -0800

    fix typo

--- js/testing/etc/config.playnet.js
+++ js/testing/etc/config.playnet.js
@@ -117,7 +117,7 @@ function appConfig (id, peer) {
       args: ['--window-size=1200,800'],
       defaultViewport: { width: 1200, height: 800 },
       headless: false,
-      sloMo: 100,
+      slowMo: 100,
       timeout: 30000
     }
   }
